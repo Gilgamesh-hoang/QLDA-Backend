a394fb1a91c7bb5392f37aac6a382b099f7a0bad
Quancon <21130265@st.hcmuaf.edu.vn>
2024-10-05 21:07:57 +0700

    messege

--- src/main/java/com/commic/v1/entities/ChapterContent.java
+++ src/main/java/com/commic/v1/entities/ChapterContent.java
@@ -1,5 +1,16 @@
 package com.commic.v1.entities;
-
+/**
+ * Add ChapterContent Entity with Relationships and Attributes
+ *
+ * Description:
+ * - Đã tạo thực thể ChapterContent trong gói entities.
+ * - Thực thể chứa các trường: id, chapter (quan hệ nhiều-một với Chapter),
+ *   linkImage, và trường isDeleted để đánh dấu nội dung đã xóa.
+ * - Sử dụng Lombok để tự động tạo các phương thức getter và constructor không tham số.
+ * - Đặt giá trị mặc định cho trường isDeleted là false.
+ *
+ * Tags: #entity #JPA #Lombok #Java #Hibernate
+ */
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
