6e83c30437698147b278456abb0fde7dca890c9e
LKVan <20130462@st.hcmuaf.edu.vn>
2024-10-07 11:57:58 +0700

    add reponse dto repositories and service for category

--- src/main/java/com/commic/CommicApplication.java
+++ src/main/java/com/commic/CommicApplication.java
@@ -2,8 +2,10 @@ package com.commic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.commic.v1.mapper")
 public class CommicApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommicApplication.class, args);
