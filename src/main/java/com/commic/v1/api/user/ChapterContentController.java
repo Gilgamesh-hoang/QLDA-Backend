b3b6fae51bbce592d8b44cc45674a9a7079fdef5
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-10-12 01:48:04 +0700

    update: small changes in controller, now using Constructor-based Autowired for injecting beans instead of Field Inject (not recommend way in new spring boot version).

--- src/main/java/com/commic/v1/api/user/ChapterContentController.java
+++ src/main/java/com/commic/v1/api/user/ChapterContentController.java
@@ -4,6 +4,9 @@ import com.commic.v1.dto.responses.APIResponse;
 import com.commic.v1.dto.responses.ChapterContentResponse;
 import com.commic.v1.exception.ErrorCode;
 import com.commic.v1.services.chapter.IChapterContentService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +17,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ChapterContentController {
-    private IChapterContentService chapterContentService; // TODO: add implementation bean
+    IChapterContentService chapterContentService;
 
     @GetMapping(value = "/chapter-{id}")
     public APIResponse<List<ChapterContentResponse>> getChapterContent(@PathVariable Integer id) {
