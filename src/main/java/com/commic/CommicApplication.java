36f75ddee97fdd3e977f39cafff75fe7a617a2e1
LKVan <20130462@st.hcmuaf.edu.vn>
2024-10-14 22:04:12 +0700

    update category

--- src/main/java/com/commic/CommicApplication.java
+++ src/main/java/com/commic/CommicApplication.java
@@ -2,10 +2,8 @@ package com.commic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.commic.v1.mapper")
 public class CommicApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommicApplication.class, args);
