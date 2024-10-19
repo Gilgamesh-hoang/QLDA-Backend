d3685ebeff8a571f9c16ed808a5321201905806f
Lê Anh Đức <ducvui2003@gmail.com>
2024-10-19 19:02:31 +0700

    Hotfix/category (#9)
    * hotfix: add rank and category comic
    
    * write unit test
    
    ---------
    
    Co-authored-by: KHAC NHUAN <20130090@st.hcmuaf.edu.vn>

--- src/main/java/com/commic/v1/api/user/BookController.java
+++ src/main/java/com/commic/v1/api/user/BookController.java
@@ -6,12 +6,12 @@
  */
 package com.commic.v1.api.user;
 
-import com.commic.v1.services.book.IBookService;
 import com.commic.v1.dto.DataListResponse;
 import com.commic.v1.dto.responses.APIResponse;
 import com.commic.v1.dto.responses.BookResponse;
 import com.commic.v1.entities.Book;
 import com.commic.v1.exception.ErrorCode;
+import com.commic.v1.services.book.IBookService;
 import com.commic.v1.services.search.ISearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
